e7e75c005ee3141c73c33ae39aeeffb2fb7d4f85
Hussein Fakhreddine <hfakhreddine86@gmail.com>
2018-01-11 21:24:22 -0800

    Frontend Changes

--- client/src/Views/Home/Home.js
+++ client/src/Views/Home/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Feed from '../../Components/Feed';
-import Comments from '../../Components/Comments';
 import { Container } from 'semantic-ui-react'
 
 
 const Home = () => (
     <Container>
         <Feed />
-        <Comments />
     </Container>
 )
 
