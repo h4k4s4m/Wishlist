589e4054284c83c1326b0c69cf57a2f098649348
gitrenn <rjingmembers@gmail.com>
2018-01-17 15:36:57 -0800

    hashed password and authenticated user login

--- client/src/Data/user-api.js
+++ client/src/Data/user-api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 const userApi = {
+  login: credential => axios.post('/api/login', credential).then(data => {
+     console.log(`this is a test: ${data}`);
+     return data;
+  }),
   getAll: () => axios.get('/api/user').then(results => results.data),
   getById: id => axios.get(`/api/user/${id}`).then(results => results.data),
   create: account => axios.post('/api/account', account).then(results => results.data),
--- server/models/account.js
+++ server/models/account.js
@@ -24,14 +24,42 @@ module.exports = function (sequelize, DataTypes) {
     }
   });
 
+  Account.hook("beforeCreate",  function(account) {
+    account.password = bcrypt.hashSync(account.password, bcrypt.genSaltSync(10), null);
+  });
   // Creating a custom method for our Account model. This will check if an unhashed password entered by the Account can be compared to the hashed password stored in our database
-  Account.prototype.validPassword =  (password) => bcrypt.compareSync(password, this.password);
+  Account.prototype.validPassword =  function(password) { 
+    return bcrypt.compareSync(password, this.password);
+  };
   
   // Hooks are automatic methods that run during various phases of the Account Model lifecycle
   // In this case, before an Account is created, we will automatically hash their password
-  Account.hook("beforeCreate",  (account) => {
-    account.password = bcrypt.hashSync(account.password, bcrypt.genSaltSync(10), null);
-  });
+  
+//   Account.beforeCreate(function(account, options) {
+//     return cryptPassword(account.password)
+//       .then(success => {
+//         account.password = success;
+//       })
+//       .catch(err => {
+//         if (err) console.log(err);
+//       });
+//   });
+
+// function cryptPassword(password) {
+//   console.log("cryptPassword" + password);
+//   return new Promise(function(resolve, reject) {
+//     bcrypt.genSalt(10, function(err, salt) {
+//       // Encrypt password using bycrpt module
+//       if (err) return reject(err);
+
+//       bcrypt.hash(password, salt, null, function(err, hash) {
+//         if (err) return reject(err);
+//         return resolve(hash);
+//       });
+//     });
+//   });
+// };
+ 
 
   return Account;
 };
