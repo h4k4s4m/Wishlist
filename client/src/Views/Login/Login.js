bc333e2cfe998a0032d8e6d57663f57509a5627f
Hussein Fakhreddine <hfakhreddine86@gmail.com>
2018-01-19 10:52:31 -0800

    fixed conflicts

--- client/src/Views/Login/Login.js
+++ client/src/Views/Login/Login.js
@@ -8,7 +8,7 @@ class Login extends Component {
     loading: true,
     credential: {}
   };
-  history=this.props.history;
+  history = this.props.history;
   loginUser = () => {
     userApi.login(this.state.credential)
       .then(loggedin => {
@@ -47,60 +47,60 @@ class Login extends Component {
   render() {
     return (
       <div className='login-form'>
-      <Container text style={{ marginTop: '7em' }}>
-    {/*
+        <Container text style={{ marginTop: '7em' }}>
+          {/*
         Heads up! The styles below are necessary for the correct render of this example.
         You can do same with CSS, the main idea is that all the elements up to the `Grid`
         below must have a height of 100%.
       */}
-      <style>{`
+          <style>{`
         body > div,
         body > div > div,
         body > div > div > div.login-form {
         }
       `}</style>
-      <Grid
-        textAlign='center'
-        verticalAlign='middle'
-      >
-        <Grid.Column style={{ maxWidth: 450 }}>
-       <Header as='h2' color='blue' textAlign='center'>
-         {' '}Login to Your Account
+          <Grid
+            textAlign='center'
+            verticalAlign='middle'
+          >
+            <Grid.Column style={{ maxWidth: 450 }}>
+              <Header as='h2' color='blue' textAlign='center'>
+                {' '}Login to Your Account
          </Header>
-       <Form size='large'>
-         <Segment stacked>
-           <Form.Input
-             onChange={this.handleInputChange}
-             value={this.state.credential.username}
-             name='username'
-             fluid
-             icon='user'
-             iconPosition='left'
-             placeholder='Username'
-           />
-           <Form.Input
-             onChange={this.handleInputChange}
-             value={this.state.credential.password}
-             name='password'
-             fluid
-             icon='lock'
-             iconPosition='left'
-             placeholder='Password'
-             type='password'
-           />
+              <Form size='large'>
+                <Segment stacked>
+                  <Form.Input
+                    onChange={this.handleInputChange}
+                    value={this.state.credential.username}
+                    name='username'
+                    fluid
+                    icon='user'
+                    iconPosition='left'
+                    placeholder='Username'
+                  />
+                  <Form.Input
+                    onChange={this.handleInputChange}
+                    value={this.state.credential.password}
+                    name='password'
+                    fluid
+                    icon='lock'
+                    iconPosition='left'
+                    placeholder='Password'
+                    type='password'
+                  />
 
-           <Button onClick={this.loginUser} primary fluid size='large'>
-             Login
-           </Button>
-         </Segment>
-       </Form>
-       <Message>
-         New to us? <a href='/signup'>Sign Up</a>
-       </Message>
-     </Grid.Column>
-    </Grid>
-    </Container>
-  </div>
+                  <Button onClick={this.loginUser} primary fluid size='large'>
+                    Login
+                </Button>
+                </Segment>
+              </Form>
+              <Message>
+                New to us? <a href='/signup'>Sign Up</a>
+              </Message>
+            </Grid.Column>
+          </Grid>
+        </Container>
+      </div>
     )
   }
 }
