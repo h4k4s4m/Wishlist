0b7a304cdeaea19362e8cebf5a19286c1aa3cbdd
Hussein Fakhreddine <hfakhreddine86@gmail.com>
2018-01-19 13:37:36 -0800

    Profile dynamic changes

--- client/src/Components/ProfileCard/Card.js
+++ client/src/Components/ProfileCard/Card.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { Card, Icon, Image } from 'semantic-ui-react';
+import { Card, Image } from 'semantic-ui-react';
 
-const MyCard = () => (
+const MyCard = props => (
   <Card>
     <Image src='/assets/images/tom.jpg' />
     <Card.Content>
-      <Card.Header>Tom</Card.Header>
+      <Card.Header>{props.fullName}</Card.Header>
       <Card.Meta>Joined in 2016</Card.Meta>
-      <Card.Description>Tom is a comedian living in Nashville.</Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        10 Friends
-      </a>
     </Card.Content>
   </Card>
 )
--- client/src/Views/Profile/Profile.js
+++ client/src/Views/Profile/Profile.js
@@ -1,47 +1,51 @@
 import React, { Component } from 'react';
 import MyCard from '../../Components/ProfileCard';
-import { Container, Grid, Segment, Button } from 'semantic-ui-react'
+import { Container, Grid, Segment } from 'semantic-ui-react'
 
 class Profile extends Component {
     state = {
         loading: true,
         account: {}
     };
-   
-  componentDidMount = () =>{
-    this.setState({
-        account:{
-            username: sessionStorage.getItem('userName'),
-            fullName: sessionStorage.getItem('fullName')
-        }
-    })
-  }
-      render() {
-          return (
+
+    componentDidMount = () => {
+        this.setState({
+            account: {
+                username: sessionStorage.getItem('userName'),
+                fullName: sessionStorage.getItem('fullName')
+            }
+        })
+    }
+    render() {
+        return (
             <Container text style={{ marginTop: '7em' }}>
-        <Grid columns={2} divided>
-            <Grid.Row stretched>
-                <Grid.Column mobile={16} tablet={8} computer={6}>
-                    <Segment textAlign='center' color='blue'>
-                        <MyCard />
-                    </Segment>
-                </Grid.Column>
-                <Grid.Column mobile={16} tablet={8} computer={10}>
-                    <Segment textAlign='center' color='blue'>
-                        <h1>Profile</h1>
-                        <hr />
-                        <h5>Name: { this.state.account.fullName }</h5>
-                        <h5>Email: tom@gmail.com</h5>
-                        <h5>Password: **********</h5>
-                        <h5>DOB: 1990-03-31</h5>
-                        <h5>Wish: New Hat</h5>
-                    </Segment>
-                </Grid.Column>
-            </Grid.Row>
-        </Grid>
-    </Container>
-          )
-      }
+                <Grid columns={3} divided>
+                    <Grid.Row stretched>
+                        <Grid.Column mobile={16} tablet={8} computer={4}>
+                            <Segment textAlign='center' color='blue'>
+                                <MyCard fullName={this.state.account.fullName} />
+                            </Segment>
+                        </Grid.Column>
+                        <Grid.Column mobile={16} tablet={8} computer={6}>
+                            <Segment textAlign='center' color='blue'>
+                                <h1>Profile Details</h1>
+                                <hr />
+                                <h5>Username:</h5>
+                                <h5>Email: </h5>
+                                <h5>DOB: 1990-03-31</h5>
+                            </Segment>
+                        </Grid.Column>
+                        <Grid.Column mobile={16} tablet={8} computer={6}>
+                            <Segment textAlign='center' color='blue'>
+                                <h1>Wishlist Items</h1>
+                                <hr />
+                            </Segment>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Container>
+        )
+    }
 }// end of class
 
 export default Profile;
