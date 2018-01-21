03d14f5ad4fbb192ff87cb3e51288b12dc3906fc
Sahm Samarghandi <h4k4s4m@gmail.com>
2018-01-21 10:29:37 -0800

    early comment work

--- client/src/Components/Feed/Feed.js
+++ client/src/Components/Feed/Feed.js
@@ -3,8 +3,10 @@ import 'semantic-ui-css/semantic.min.css';
 import { Grid, Segment, Container } from 'semantic-ui-react';
 import FeedEvent from "../FeedEvent";
 import SearchBar from "../SearchBar";
+import Comment from "../Comments"
 
 const MyFeed = () => (
+<<<<<<< Q
     <Container>
         <Grid columns={2} divided>
             <Grid.Row>
@@ -23,6 +25,27 @@ const MyFeed = () => (
             </Grid.Row>
         </Grid>
     </Container>
+=======
+    <Grid columns={2} divided>
+        <Grid.Row>
+            <Grid.Column mobile={16} tablet={16} computer={16}>
+                <Segment color='blue'>
+                    <SearchBar />
+                </Segment>
+            </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+            <Grid.Column mobile={16} tablet={16} computer={16}>
+                <Segment color='blue'>
+                    <FeedEvent who={"Sahm"} what={"added a gift"} when={"Like 15 minutes ago"} />
+                    <FeedEvent who={"Ren"} what={"liked a gift"} when={"Like 20 minutes ago"} />
+                    <FeedEvent who={"Q"} what={"stole a gift"} when={"Like 15 minutes ago"} />
+                    <Comment />
+                </Segment>
+            </Grid.Column>
+        </Grid.Row>
+    </Grid>
+>>>>>>> early comment work
 )
 
 export default MyFeed;
