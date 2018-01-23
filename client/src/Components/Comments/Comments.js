11e4724805bb16bfc986ce9dcb4e53c4fc411458
Unknown <quentinsitunes@yahoo.com>
2018-01-23 14:09:41 -0800

    Auto stash before merge of "master" and "origin/master"

--- client/src/Components/Comments/Comments.js
+++ client/src/Components/Comments/Comments.js
@@ -4,7 +4,27 @@ import userApi from '../../Data/user-api';
 
 export default class WishComment extends React.Component{
 state = {
-  name: ""
+  name: "",
+  isClicked: false
+}
+
+handleClick =  () => {
+  let temp = !this.state.isClicked;
+  console.log(temp);
+  this.setState({
+    isClicked: temp
+  })
+}
+
+conditionalRender = (props) => {
+  if(props){
+    return(
+      <Form reply>
+      <Form.TextArea />
+      <Button content='Add Reply' labelPosition='left' icon='edit' primary />
+    </Form> 
+    )
+  }
 }
 
 userLookup = (id) =>{
@@ -28,9 +48,11 @@ render(){
         </Comment.Metadata>
         <Comment.Text>{this.props.text}!</Comment.Text>
         <Comment.Actions>
-          <Comment.Action>Reply</Comment.Action>
+          <Comment.Action onClick={this.handleClick}>Reply</Comment.Action>
         </Comment.Actions>
       </Comment.Content>
+      {this.conditionalRender}
     </Comment>
+
   )}
 }
